bb97bff8115dae0e7c654bb61e2a4bfabc46d3b3
juliyaaj <160793723+juliyaaj@users.noreply.github.com>
2024-03-11 18:34:47 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# secret-python-bu3ym3
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/juliyaaj/secret-python-bu3ym3)
---
+++ main.py
@@ -0,0 +1,11 @@
+#Jorrey, Juliya
+
+ def sales_projection(sales, rate, years):
+    # my sales projection calculation here
+    return(sales + rate + years)
+
+answer = sales_projection(3, 7)
+print(answer)
+
+
+print("hello world")
